25841e655b57b62bbc681979da71a8e23bf6f049
Daxcer <daxcer376@gmail.com>
2024-10-25 08:54:37 -0600

    Se actualiza el webscraping para cambiar a númerico las columnas necesarias y se mejoran los queries

--- datos/data_sqlite3.py
+++ datos/data_sqlite3.py
@@ -1,28 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import pandas as pd
 import sqlite3
-import time
 
-# Inicializamos el WebDriver
+# Iniciamos el WebDriver
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
-# Conectamos a la base de datos SQLite (si no existe, se crea automáticamente)
+# Conectamos a la base de datos SQLite (Si no existe se crea una)
 conn = sqlite3.connect('C:/Users/danag.LAPTOP-A0ADBJQ7/Downloads/Clases_Diplomado/proyecto_final/datos/data.db')
 
-# Lista de temporadas
+# Lista de temporadas a considerar (Son todas las que estan disponibles en FBREF)
 seasons = ['2024-2025', '2023-2024', '2022-2023', '2021-2022', '2020-2021', '2019-2020', '2018-2019']
 
-# URLs para cada estadística
+# Se crea un diccionario con el ID de la tabla y su URL
 urls = {
     'stats_standard': 'https://fbref.com/en/comps/9/{}/stats/Premier-League-Stats',
     'stats_keeper': 'https://fbref.com/en/comps/9/{}/keepers/Premier-League-Stats',
@@ -37,49 +34,71 @@ urls = {
     'stats_misc': 'https://fbref.com/en/comps/9/{}/misc/Premier-League-Stats'
 }
 
-# Función para cargar una tabla de una URL y un ID específico
+# Definimos la función para cargar la tabla toamando el ID y el URL
 def load_table(url, table_id):
-    driver.get(url)
-    # Esperamos hasta que la tabla se cargue completamente
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, table_id)))
-    # Parseamos el contenido con BeautifulSoup
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    
-    # Buscamos la tabla por su ID
-    table = soup.find('table', {'id': table_id})
-    
-    # Convertimos la tabla en un DataFrame de pandas
-    if table:
-        df = pd.read_html(str(table))[0]
-        
-        # Si existe un MultiIndex, concatenamos los niveles del índice para formar nombres únicos
-        if isinstance(df.columns, pd.MultiIndex):
-            df.columns = ['_'.join(filter(None, col)).strip() for col in df.columns]
-        
-        # Imprimir los nombres de las columnas para revisar
-        print(f'Columnas en {table_id}: {df.columns.tolist()}')
-        return df
-    else:
-        return None
+  driver.get(url)
+  WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, table_id)))
+  soup = BeautifulSoup(driver.page_source, 'html.parser')
+  table = soup.find(id=table_id)
+
+  # Ajustamos los MultiIndex en los nombres de la columnas y convertimos los valores a numéricos
+  if table:
+    df = pd.read_html(str(table))[0]
+
+    if isinstance(df.columns, pd.MultiIndex):
+      df.columns = ['_'.join(filter(None, col)).strip() for col in df.columns]
+
+    # Definir las columnas que deben ser texto
+    text_columns = [
+            'Unnamed: 0_level_0_Rk', 'Unnamed: 1_level_0_Player',
+            'Unnamed: 2_level_0_Nation', 'Unnamed: 3_level_0_Pos',
+            'Unnamed: 4_level_0_Squad', 'Unnamed: 5_level_0_Age',
+            'Unnamed: 6_level_0_Born'
+        ]
+
+    # Renombrar las columnas eliminando el prefijo 'Unnamed: ...'
+    new_column_names = {
+            'Unnamed: 0_level_0_Rk': 'Rk',
+            'Unnamed: 1_level_0_Player': 'Player',
+            'Unnamed: 2_level_0_Nation': 'Nation',
+            'Unnamed: 3_level_0_Pos': 'Pos',
+            'Unnamed: 4_level_0_Squad': 'Squad',
+            'Unnamed: 5_level_0_Age': 'Age',
+            'Unnamed: 6_level_0_Born': 'Born'
+        }
+    df.rename(columns=new_column_names, inplace=True)
+
+    for column in df.columns:
+      if column not in new_column_names.values():
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+
+    # Eliminar la última columna si contiene 'Matches' en su nombre
+    last_column = df.columns[-1]
+    if 'Matches' in last_column:
+        df.drop(columns=[last_column], inplace=True)
+
+    print(f'Columnas en {table_id}: {df.columns.tolist()}')
+    return df
+
+  else:
+    print(f'No se encontró la tabla con el ID {table_id}')
+    return None
 
 # Iteramos sobre las temporadas
 for season in seasons:
-    # Iteramos sobre las URLs de cada estadística
-    for stat_name, stat_url in urls.items():
-        formatted_url = stat_url.format(season)  # Reemplazamos con la temporada
-        # El ID de la tabla es el nombre de la estadística (ej. 'stats_shooting')
-        table_id = stat_name
-        # Cargamos la tabla
-        print(f'Cargando datos de {stat_name} para la temporada {season}...')
-        df = load_table(formatted_url, table_id)
-        
-        if df is not None:
-            # Guardamos el DataFrame en la base de datos SQLite
-            table_name = f'{season}_{table_id}'
-            df.to_sql(table_name, conn, if_exists='replace', index=False)
-            print(f'Datos de {table_name} guardados en la base de datos.')
+  for stat_name, stat_url in urls.items():
+    formatted_url = stat_url.format(season)
+    table_id = stat_name
+    df = load_table(formatted_url, table_id)
 
-# Cerramos la conexión a la base de datos y el WebDriver
+    if df is not None:
+      table_name = f'{season}_{table_id}'
+      df.to_sql(table_name, conn, if_exists='replace', index=False)
+      print(f'Datos de {table_name} guardados en la base de datos.')
+    else:
+      print(f'No se pudo obtener la tabla para {table_name}.')
+
+# Cerramos la conección a la base de datos y el WebDriver
 conn.close()
 driver.quit()
 print("Proceso completado, datos guardados en data.db")
--- datos/db_queries.py
+++ datos/db_queries.py
@@ -1,38 +1,74 @@
 import sqlite3
+
+# Hacemos la conexión con la base de datos
 def connect_db(db_name):
-    return sqlite3.connect(db_name)
+  return sqlite3.connect(db_name)
 
+# Consultamos los nombres de las tablas
 def list_tables(conn):
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cursor.fetchall()
-    return [table[0] for table in tables]
+  cursor = conn.cursor()
+  cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+  tables = cursor.fetchall()
+  return [table[0] for table in tables]
 
+# Consultamos los nombres de las columnas de una tabla a elección
 def list_columns(conn, table_name):
-    cursor = conn.cursor()
-    # Usando comillas dobles alrededor del nombre de la tabla
-    cursor.execute(f'PRAGMA table_info("{table_name}");')
-    columns = cursor.fetchall()
-    return [column[1] for column in columns]
+  cursor = conn.cursor()
+  cursor.execute(f'PRAGMA table_info("{table_name}");')
+  columns = cursor.fetchall()
+  return [column[1] for column in columns]
 
-def get_player_names(conn, table_name, column_name, limit=10):
-    cursor = conn.cursor()
-    # Asegúrate de que el nombre de la columna es correcto
-    cursor.execute(f'SELECT "{column_name}" FROM "{table_name}" LIMIT {limit};')  
-    players = cursor.fetchall()
-    return [player[0] for player in players]
+# Consultamos los primeros 10 datos de una columna
+def get_data(conn, table_name, column_name, limit=10):
+  cursor = conn.cursor()
+  cursor.execute(f'SELECT "{column_name}" FROM "{table_name}" LIMIT {limit};')
+  data = cursor.fetchall()
+  return [row[0] for row in data]
 
-if __name__ == "__main__":
-    conn = connect_db('C:/Users/danag.LAPTOP-A0ADBJQ7/Downloads/Clases_Diplomado/proyecto_final/datos/data.db')
-    tables = list_tables(conn)
-    print("Tablas disponibles:")
-    for table in tables:
-        print(table)
+# Consultamos información de los datos
+def get_data_info(conn, table_name):
+  cursor = conn.cursor()
 
-    # Cambia aquí el nombre de la tabla que quieres consultar
-    table_name = '2024-2025_stats_passing'  
-    columns = list_columns(conn, table_name)
-    print(f"\nColumnas en la tabla {table_name}:")
-    for column in columns:
-        print(column)
-    conn.close()
+  # Obtener los nombres de las columnas y sus tipos de datos
+  cursor.execute(f'PRAGMA table_info("{table_name}");')
+  column_info = cursor.fetchall()
+  column_names = [row[1] for row in column_info]
+  column_types = [row[2] for row in column_info]
+
+  # Contar el número de filas
+  cursor.execute(f'SELECT COUNT(*) FROM "{table_name}";')
+  num_rows = cursor.fetchall()
+
+  # Imprimimos la información
+  print(f'\nInformación de la tabla {table_name}')
+  print('Número de filas:', num_rows)
+  print('Columnas:')
+  for name, type in zip(column_names, column_types):
+    print(f'  {name} ({type})')
+
+# Mostramos los nombres de las tablas
+if __name__ == '__main__':
+  conn = connect_db('C:/Users/danag.LAPTOP-A0ADBJQ7/Downloads/Clases_Diplomado/proyecto_final/datos/data.db')
+  tables = list_tables(conn)
+  for table in tables:
+    print(table)
+
+# Mostramos los nombres de las columnas de la tabla elegida
+table_name = '2024-2025_stats_standard'
+columns = list_columns(conn, table_name)
+print(f"\nColumnas de la tabla {table_name}:")
+for column in columns:
+  print(column)
+
+# Mostramos los primeros 10 datos de una columna
+column_name = 'Nation'
+data = get_data(conn, table_name, column_name, limit=10)
+print(f"\nPrimeras 10 filas de la columna {column_name}:")
+for row in data:
+  print(row)
+
+# Mostramos información de los datos
+data_info = get_data_info(conn, table_name)
+print(data_info)
+
+conn.close()
