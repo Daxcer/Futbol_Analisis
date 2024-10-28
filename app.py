5e974baedefd6670fb89640b12e96713583f66a3
Daxcer <daxcer376@gmail.com>
2024-10-28 09:07:21 -0600

    Se actualiza la navegación de app.py y los requirements para el modelo

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ pages = {
     "Navegación": [ 
         st.Page("contenido/inicio.py", title="Inicio"), 
         st.Page("contenido/analisis_de_jugadores.py", title="Análisis de los Jugadores"), 
-        st.Page("contenido/analisis_por_equipo.py", title="Análisis por Equipo"), 
-        st.Page("contenido/comparacion_entre_equipos.py", title="Comparación entre Equipos"), 
-        st.Page("contenido/global_liga.py", title="Global Liga") 
     ]  
 }
 
